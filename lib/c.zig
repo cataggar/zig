901699dd4d314ec804221fb7c12f843b9d079c83
Cameron Taggart <cameron.taggart@gmail.com>
2026-03-27 08:42:17 -0400

    libzigc: migrate env __environ, exit abort_lock

--- lib/c.zig
+++ lib/c.zig
@@ -81,6 +81,8 @@ comptime {
     _ = @import("c/sys/reboot.zig");
     _ = @import("c/sys/utsname.zig");
 
+    _ = @import("c/env.zig");
+    _ = @import("c/exit.zig");
     _ = @import("c/unistd.zig");
     _ = @import("c/wchar.zig");
 }
---
+++ lib/c/env.zig
@@ -0,0 +1,12 @@
+const builtin = @import("builtin");
+
+var environ_var: ?[*:null]?[*:0]u8 = null;
+
+comptime {
+    if (builtin.target.isMuslLibC()) {
+        @export(&environ_var, .{ .name = "__environ", .linkage = .weak, .visibility = .hidden });
+        @export(&environ_var, .{ .name = "___environ", .linkage = .weak, .visibility = .hidden });
+        @export(&environ_var, .{ .name = "_environ", .linkage = .weak, .visibility = .hidden });
+        @export(&environ_var, .{ .name = "environ", .linkage = .weak, .visibility = .hidden });
+    }
+}
