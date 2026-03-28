114b871d533c359b711f78f70a26d7ac7bcd7631
Cameron Taggart <cameron.taggart@gmail.com>
2026-03-28 11:36:36 -0400

    libzigc: migrate misc wordexp, wordfree

--- src/libs/musl.zig
+++ src/libs/musl.zig
@@ -1086,7 +1086,6 @@ const src_files = [_][]const u8{
     "musl/src/misc/setrlimit.c",
     "musl/src/misc/syscall.c",
     "musl/src/misc/syslog.c",
-    "musl/src/misc/wordexp.c",
     "musl/src/mman/mmap.c",
     "musl/src/mman/mremap.c",
     "musl/src/mman/msync.c",
