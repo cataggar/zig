b1a7cd7a49ce47f4f13c905a92c21a196c59105a
Cameron Taggart <cameron.taggart@gmail.com>
2026-04-02 11:36:27 +0100

    libzigc: migrate remaining misc, env, mman, sched functions to Zig
    Migrate 23 C files to Zig implementations:
    
    mman (3): mmap, msync, munmap - syscall wrappers with MAP_FAILED
      handling and EPERM-to-ENOMEM fixup for anonymous mappings
    
    sched (8): sched_yield, sched_get_priority_max/min, sched_getparam,
      sched_setparam, sched_getscheduler, sched_setscheduler,
      sched_rr_get_interval, __sched_cpucount
    
    misc (12): getresuid, getresgid, getentropy, gethostid, getpriority,
      setpriority, setdomainname, getrlimit, getrusage, a64l/l64a,
      basename/dirname
    
    Pure computation functions (a64l, l64a, basename, dirname, gethostid)
    are also exported for WASI targets.

--- lib/c/sys/mman.zig
+++ lib/c/sys/mman.zig
@@ -15,11 +15,20 @@ comptime {
         symbol(&mlockLinux, "mlock");
         symbol(&mlockallLinux, "mlockall");
 
+        symbol(&mmapLinux, "mmap");
+        symbol(&mmapLinux, "__mmap");
+        symbol(&mmapLinux, "mmap64");
+
         symbol(&mprotectLinux, "mprotect");
         symbol(&mprotectLinux, "__mprotect");
 
+        symbol(&msyncLinux, "msync");
+
         symbol(&munlockLinux, "munlock");
         symbol(&munlockallLinux, "munlockall");
+
+        symbol(&munmapLinux, "munmap");
+        symbol(&munmapLinux, "__munmap");
 
         symbol(&posix_madviseLinux, "posix_madvise");
     }
@@ -59,4 +68,38 @@ fn munlockallLinux() callconv(.c) c_int {
 fn posix_madviseLinux(addr: *anyopaque, len: usize, advice: c_int) callconv(.c) c_int {
     if (advice == std.os.linux.MADV.DONTNEED) return 0;
     return @intCast(-@as(isize, @bitCast(std.os.linux.madvise(@ptrCast(addr), len, @bitCast(advice)))));
+}
+
+const linux = std.os.linux;
+
+const MAP_FAILED: ?*anyopaque = @ptrFromInt(std.math.maxInt(usize));
+
+fn mmapLinux(addr: ?*anyopaque, len: usize, prot: c_int, flags: c_int, fd: c_int, off: i64) callconv(.c) ?*anyopaque {
+    // Reject mappings that would overflow ptrdiff_t
+    if (len >= @as(usize, @intCast(std.math.maxInt(isize)))) {
+        std.c._errno().* = @intFromEnum(linux.E.NOMEM);
+        return MAP_FAILED;
+    }
+    const ret = linux.mmap(@ptrCast(addr), len, @bitCast(@as(u32, @bitCast(prot))), @bitCast(@as(u32, @bitCast(flags))), fd, off);
+    const signed: isize = @bitCast(ret);
+    if (signed < 0 and signed >= -4095) {
+        @branchHint(.unlikely);
+        var e: c_int = @intCast(-signed);
+        // Fixup incorrect EPERM from kernel for anonymous mappings
+        if (e == @intFromEnum(linux.E.PERM) and addr == null) {
+            const f: linux.MAP = @bitCast(@as(u32, @bitCast(flags)));
+            if (f.ANONYMOUS and !f.FIXED) e = @intFromEnum(linux.E.NOMEM);
+        }
+        std.c._errno().* = e;
+        return MAP_FAILED;
+    }
+    return @ptrFromInt(ret);
+}
+
+fn msyncLinux(addr: *anyopaque, len: usize, flags: c_int) callconv(.c) c_int {
+    return errno(linux.msync(@ptrCast(addr), len, flags));
+}
+
+fn munmapLinux(addr: *anyopaque, len: usize) callconv(.c) c_int {
+    return errno(linux.munmap(@ptrCast(addr), len));
 }
